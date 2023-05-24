5e9d142927cac91a748d9cdddcc741d87343963a
sermarvin <marv.kariuki@gmail.com>
2023-05-24 22:11:46 +0530

    main shell file

---
+++ shell.c
@@ -0,0 +1,50 @@
+#include "shell.h"
+
+/**
+ * main - creates a shell program
+ *
+ * Return: Nothing (0)
+ */
+int main(void)
+{
+	char *prompt = "#myShell$ ", *buffer = NULL, **args, *path;
+	size_t size = BUFF_SIZE;
+	ssize_t str_read;
+	bool pipe_input = false;
+	pid_t pid;
+
+	while (1 && !pipe_input)
+	{
+		if (isatty(STDIN_FILENO) == 0)
+			pipe_input = true;
+		else
+			write(STDOUT_FILENO, prompt, 10);
+		str_read = getline(&buffer, &size, stdin);
+		if (str_read == -1)
+		{
+			perror("Error");
+			free(buffer);
+			exit(1);
+		}
+		if (buffer[str_read - 1] == '\n')
+			buffer[str_read - 1] = '\0';
+
+		args = split_tokens(buffer);
+		path = args[0];
+		pid = fork();
+		if (pid == 0)
+		{
+			if ((execve(path, args, NULL)) == -1)
+			{
+				perror("./shell");
+				_exit(1);
+			}
+		}
+		if (pid > 0)
+		{
+			wait(NULL);
+		}
+	}
+	free(buffer);
+	return (0);
+}
