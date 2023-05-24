1877b8115c5a9674cfaaf3ee9a3d675d49ba2c5d
Kanore <derro91@gmail.com>
2023-05-24 12:21:13 -0700

    Authors

--- _strtok.c
+++ _strtok.c
@@ -12,20 +12,22 @@ char **split_tokens(char *str)
 	char *token;
 	char **arguments;
 	int counter = 0;
-	char *d = " ";
+	char *d = " \t\n";
 
-	arguments = malloc(sizeof(char) * MAX_ARGS);
+	arguments = malloc(sizeof(char *) * (MAX_ARGS + 1));
 	if (arguments == NULL)
 	{
-		free(arguments);
+		perror("malloc error");
+		exit(EXIT_FAILURE);
 	}
 
 	token = strtok(str, d);
-	while (token != NULL && counter < MAX_ARGS - 1)
+	while (token != NULL && counter < MAX_ARGS)
 	{
 		arguments[counter++] = token;
 		token = strtok(NULL, d);
 	}
 	arguments[counter] = NULL;
-	return (arguments);
+	
+	return arguments;
 }
