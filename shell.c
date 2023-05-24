e15befdc9d8d5bbe5f88329297a5f1704632d686
Kanore <derro91@gmail.com>
2023-05-24 15:52:04 -0700

    New Files

--- shell.c
+++ shell.c
@@ -1,118 +1,52 @@
 #include "shell.h"
-
- /**
+/**
  * sig_handler - checks if Ctrl C is pressed
  * @sig_num: int
  */
-
 void sig_handler(int sig_num)
 {
 	if (sig_num == SIGINT)
 	{
-		_puts("\n#cisfun$ ");
+		write(1, "\n$ ", 3);
 	}
 }
-
- /**
- * _EOF - handles the End of File
- * @len: return value of getline function
- * @buff: buffer
- */
-
-void _EOF(int len, char *buff)
-{
-	(void)buff;
-	if (len == -1)
-	{
-		if (isatty(STDIN_FILENO))
-		{
-			_puts("\n");
-			free(buff);
-		}
-		exit(0);
-	}
-}
-
-
-
-
- /**
-  * _isatty - checks if terminal
-  */
-
-void _isatty(void)
-{
-	if (isatty(STDIN_FILENO))
-		_puts("#cisfun$ ");
-}
-
-
 /**
- * main - creates a shell program
- *
- * Return: Nothing (0)
+ * main - main function
+ * @argc: arg count
+ * @argv: arg vector
+ * @env: environment
+ * Return: 0 for success
  */
-
-int main(void)
+int main(int argc, char **argv, char **env)
 {
-	char *prompt = "#myShell$ ";
-	char *buffer = NULL;
-	char **args;
-	char *path;
-	size_t size = BUFF_SIZE;
-	ssize_t str_read;
-	bool pipe_input = false;
+	char *line;
+	char **dir, **cmd;
+	char *combine;
 
-	signal(SIGINT, sig_handler);
-	_isatty();
-
-	pid_t pid;
-
-	while (1 && !pipe_input)
+	if (argc == 1 || argv[1] == NULL)
 	{
-		if (isatty(STDIN_FILENO) == 0)
-			pipe_input = true;
-		else
-			write(STDOUT_FILENO, prompt, 10);
-		str_read = getline(&buffer, &size, stdin);
-		if (str_read == -1)
+		while (1)
 		{
-			perror("Error");
-			free(buffer);
-			exit(1);
-		}
-		if (buffer[str_read - 1] == '\n')
-			buffer[str_read - 1] = '\0';
-
-		args = split_tokens(buffer);
-		path = args[0];
-		
-		if (strcmp(path, "exit") == 0)
-        	{
-            		free(buffer);
-            		exit(0);
-        	}		
-
-		pid = fork();
-		if (pid == -1)
-		{
-			perror("Error");
-			exit(1);
-		}
-		if (pid == 0)
-		{
-			if (execve(path, args, NULL) == -1)
+			signal(SIGINT, sig_handler);
+			if (isatty(STDIN_FILENO))
+			write(1, "$ ", 2);
+			line = get_line();
+			if (line[0] == '\n')
 			{
-				perror("./shell");
-				free(buffer);
-				exit(1);
+				free(line);
+				continue;
 			}
+			cmd = split_line(line);
+			dir = dirTok(env);
+			combine = checkPath(dir, cmd[0]);
+			checkBuiltins(combine, cmd);
+			if (!combine)
+				perror(combine);
 		}
-		if (pid > 0)
-		{
-			wait(NULL);
-		}
+		free(line);
+		free(cmd);
+		free(dir);
 	}
-	free(buffer);
+
 	return (0);
 }
--- shell.h
+++ shell.h
@@ -1,71 +1,39 @@
-#ifndef SHELL_H
-#define SHELL_H
-
+#ifndef SHELL
+#define SHELL
+/* Library header */
+#include <string.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <stddef.h>
-#include <signal.h>
 #include <sys/wait.h>
-#include <string.h>
-#include <stdbool.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <signal.h>
 
-int _putchar(char c);
-void _puts(char *str);
-int _strlen(char *s);
-char *_strdup(char *str);
-char *concat_all(char *name, char *sep, char *value);
-
-
-char **split_tokens(char *str);
-void execute(char **argv);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+/* Marcos */
 
 extern char **environ;
+extern int dir_count;
+#define DELIM "\n\a\t "
 
+/* shell functions */
+void sig_handler(int sig_num);
+int exe(char *fullPath, char **cmd);
+int exitexe(char **cmd);
+int cd(char **cmd);
+int printenv(char **cmd);
+int checkBuiltins(char *combine, char **cmd);
+void env(char **env);
+char *get_line(void);
+char **split_line(char *line);
+char *get_env(char **env);
+char *path(char *dir, char *av);
+char **dirTok(char **env);
+char *checkPath(char **dir, char *cmd);
 
+/* string functions */
+int _strlen(char *s);
+char *_strdup(char *str);
+int _strcmp(char *s1, char *s2);
 
-
-typedef struct list_path
-{
-	char *dir;
-	struct list_path *p;
-} list_path;
-
-
-char *_getenv(const char *name);
-list_path *add_node_end(list_path **head, char *str);
-list_path *linkpath(char *path);
-char *_which(char *filename, list_path *head);
-
-/**
- * struct mybuild - pointer to function with corresponding buildin command
- * @name: buildin command
- * @func: execute the buildin command
- */
-
-typedef struct mybuild
-{
-	char *name;
-	void (*func)(char **);
-} mybuild;
-
-
-void(*checkbuild(char **arv))(char **arv);
-int _atoi(char *s);
-void exitt(char **arv);
-void env(char **arv);
-void _setenv(char **arv);
-void _unsetenv(char **arv);
-
-void freearv(char **arv);
-void free_list(list_path *head);
-
-#define BUFF_SIZE 1024
-#define MAX_ARGS 10
-
-#endif /* SHELL_H */
+#endif
