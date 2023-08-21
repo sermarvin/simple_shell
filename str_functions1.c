2f2eeae228647e7177f94b09ec4ce9c6e2a3ba7f
sermarvin <marv.kariuki@gmail.com>
2023-08-21 17:14:35 +0300

    added another string concat function

--- str_functions1.c
+++ str_functions1.c
@@ -2,6 +2,7 @@
 
 int _strlen(const char *s);
 int *_strcat(char *dest, const char *src);
+char *_strncat(char *dest, const char *src, size_t n);
 
 /**
  * _strlen - Returns length of a string
@@ -39,5 +40,24 @@ char *_strcat(char *dest, const char *src)
 	while (*srcTemp != '\0')
 		*destTemp++ = *srcTemp++;
 	*destTemp = '\0';
+	return (dest);
+}
+
+/**
+ * _strncat - concats two strings
+ * @dest: Pointer to destination string
+ * @src: Pointer to source string
+ * @n: n bytes to copy from src
+ * Return: Pointer to destination string
+ */
+char *_strncat(char *dest, const char *src, size_t n)
+{
+	size_t dest_len = _strlen(dest);
+	size_t i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[dest_len + i] = src[i];
+	dest[dest_len + i] = '\0';
+
 	return (dest);
 }
