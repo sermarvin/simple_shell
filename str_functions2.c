4939502edaa3caac6ff4bf344a75001f571e61b8
sermarvin <marv.kariuki@gmail.com>
2023-08-21 18:29:41 +0300

    added string compare function

--- str_functions2.c
+++ str_functions2.c
@@ -1,6 +1,7 @@
 #include "shell.h"
 
 int _strspn(char *s, char *accept);
+int _strncmp(const char *s1, const char *s2, size_t n);
 
 /**
  * _strspn - gets the length of a prefix substring
@@ -26,4 +27,31 @@ int _strspn(char *s, char *accept)
 		s++;
 	}
 	return (bytes);
+}
+
+/**
+ * _strncmp - compare two strings
+ * @s1: pointer to a string
+ * @s2: pointer to a string
+ * @n: the first n bytes of the strings to compare with
+ * Return: Less than 0 if s1 is shorter than s2,
+ * else, 0 if s1 and s2 match,
+ * ,Greater than 0 if s1 is longer than s2.
+ */
+int _strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t i;
+
+	for (i = 0; s1[i] && s2[i] && i < n; i++)
+	{
+		if (s1[i] > s2[i])
+			return (s1[i] - s2[i]);
+		else if (s1[i] < s2[i])
+			return (s1[i] - s2[i]);
+	}
+
+	if (i == n)
+		return (0);
+	else
+		return (-15);
 }
