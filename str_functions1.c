dc5e21b6672868ed948a6fbafffedb0e5bf4362a
sermarvin <marv.kariuki@gmail.com>
2023-08-21 17:10:52 +0300

    added string concat function

--- str_functions1.c
+++ str_functions1.c
@@ -1,13 +1,13 @@
 #include "shell.h"
 
 int _strlen(const char *s);
+int *_strcat(char *dest, const char *src);
 
 /**
  * _strlen - Returns length of a string
  * @s: pointer to the xters string
  * Return: The length of the xter string
  */
-
 int _strlen(const char *s)
 {
 	int length = 0;
@@ -17,4 +17,27 @@ int _strlen(const char *s)
 	for (length = 0; s[length]; length++)
 		;
 	return (length);
+}
+
+/**
+ * _strcat - concats two strings.
+ * @dest: Pointer to destination string
+ * @src: Pointer to source string
+ * Return: Pointer to destination string
+ */
+char *_strcat(char *dest, const char *src)
+{
+	char *destTemp;
+	const char *srcTemp;
+
+	destTemp = dest;
+	srcTemp =  src;
+
+	while (*destTemp != '\0')
+		destTemp++;
+
+	while (*srcTemp != '\0')
+		*destTemp++ = *srcTemp++;
+	*destTemp = '\0';
+	return (dest);
 }
