709d1d8e2fadf6d4aff2acbaff4a1f03eb25bb89
sermarvin <marv.kariuki@gmail.com>
2023-08-21 16:20:49 +0300

    token length function

---
+++ split.c
@@ -0,0 +1,25 @@
+#include "shell.h"
+
+int token_len(char *str, char *delim);
+int count_tokens(char *str, char *delim);
+char **_strtok(char *line, char *delim);
+
+/**
+ * token_len - locates the delimeter index marking
+ * @str: the string to be searched.
+ * @delim: the delimiter character
+ * Return: The delimeter index marking
+ */
+
+
+int token_len(char *str, char *delim)
+{
+	int index = 0, len = 0;
+
+	while (*(str + index) && *(str + index) != *delim)
+	{
+		len++;
+		index++;
+	}
+	return (len);
+}
