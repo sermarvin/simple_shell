68a1fd08eae581a8649745767304ee11d248e59c
sermarvin <marv.kariuki@gmail.com>
2023-08-21 16:48:38 +0300

    added count_token function

--- split.c
+++ split.c
@@ -23,3 +23,27 @@ int token_len(char *str, char *delim)
 	}
 	return (len);
 }
+
+/**
+ * count_tokens - counts the number of delimited words
+ * @str: The string to be searched
+ * @delim: The delimeter character
+ * Return: The number of words contained within str
+ */
+
+int count_tokens(char *str, char *delim)
+{
+	int index, tokens = 0, len = 0;
+
+	for (index = 0; *(str + index); index++)
+		len++;
+	for (index = 0; index < len; index++)
+	{
+		if (*(str + index) != *delim)
+		{
+			tokens++;
+			index += token_len(str + index, delim);
+		}
+	}
+	return (tokens);
+}
